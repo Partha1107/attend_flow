c8424e34cb64520625272f7457543ee33ddc2381
karthikeyan24-kk <karthikeyan.ae.s.138@kalvium.community>
2026-08-23 06:28:37 +0530

    sending email automation

--- backend/src/routes/emailAutomationRoutes.js
+++ backend/src/routes/emailAutomationRoutes.js
@@ -5,8 +5,13 @@ const router = express.Router();
 const emailAutomationController = require("../controllers/emailAutomationControllers");
 
 router.post(
-    "/test-email",
-    emailAutomationController.sendTestEmail
+  "/test-email",
+  emailAutomationController.sendTestEmail
+);
+
+router.post(
+  "/send",
+  emailAutomationController.sendAttendanceEmail
 );
 
 module.exports = router;
